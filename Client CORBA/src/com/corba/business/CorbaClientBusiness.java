57f97883c6ea565c8192a348190099692cb305e7
marcuspimenta <mvinicius.pimenta@gmail.com>
2013-05-01 12:54:46 -0300

    Implement sample communication between client and server CORBA

---
+++ Client CORBA/src/com/corba/business/CorbaClientBusiness.java
@@ -0,0 +1,43 @@
+package com.corba.business;
+
+import com.corba.communication.CorbaClient;
+import com.corba.generatedfilesidl.HandlerMessage;
+import com.corba.generatedfilesidl.HandlerMessageHelper;
+import com.corba.generatedfilesidl.Message;
+
+/**
+ * 
+ * @author Marcus Pimenta
+ * @email mvinicius.pimenta@gmail.com
+ * @date 09:49:47 01/05/2013
+ */
+public class CorbaClientBusiness {
+	
+	private CorbaClient corbaClient;
+	private HandlerMessage handlerMessage;
+	
+	public CorbaClientBusiness(){
+		corbaClient = new CorbaClient();
+	}
+	
+	public void settingsCorbaClient(String ipServer, String nameComponent, String kindNameComponent){
+		try {
+			corbaClient.initORB(ipServer);
+			corbaClient.referencesNameService();
+			corbaClient.searchReferencesNameService(nameComponent, kindNameComponent);
+			
+			handlerMessage = HandlerMessageHelper.narrow(corbaClient.getReferenceObj());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(Message message){
+		try{
+			handlerMessage.send_msg(message);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Server CORBA/src/com/corba/business/CorbaServerBusiness.java
@@ -0,0 +1,37 @@
+package com.corba.business;
+
+import com.corba.communication.CorbaServer;
+import com.corba.domain.HandlerMessageImpl;
+
+/**
+ * 
+ * @author Marcus Pimenta
+ * @email mvinicius.pimenta@gmail.com
+ * @date 10:19:57 01/05/2013
+ */
+public class CorbaServerBusiness {
+	
+	private CorbaServer corbaServer;
+	private HandlerMessageImpl handlerMessageImpl;
+	
+	public CorbaServerBusiness(){
+		corbaServer = new CorbaServer();
+		handlerMessageImpl = new HandlerMessageImpl();
+	}
+	
+	public void settingsCorbaService(String ipServer, String nameComponent, String kindNameComponent){
+		try {
+			corbaServer.initORB(ipServer);
+			corbaServer.referencesNameService();
+			corbaServer.searchReferencesNameService(nameComponent, kindNameComponent);
+			corbaServer.referencesRootPOA();
+			
+			corbaServer.servantToRefecenreObject(handlerMessageImpl);
+			corbaServer.bindObjectNameService();
+			corbaServer.runORB();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
